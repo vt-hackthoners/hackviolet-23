43e41fde148557a681b095c01fe7800650e111ba
riddhithanki <riddhithanki@vt.edu>
2023-02-04 21:11:12 -0500

    Fetches png file from browser

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+import base64
+import os
+
 import json
 from flask import app
 from flask import request
-
+from flask_cors import CORS
 from flask import Flask, render_template
 
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+
+CORS(app)
+
+
+
+@app.route('/png', methods=['POST'])
+def png():
+    photo = request.get_json()['user_image']
+    print(photo)
+
+    return photo
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/png', methods=['POST'])
-def test():
-    output = request.get_json()
-    print(output) # This is the output that was stored in the JSON within the browser
-    print(type(output))
-    result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
-    print(type(result))#this shows the json converted as a python dictionary
-    return result
